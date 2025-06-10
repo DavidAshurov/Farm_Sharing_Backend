8857dd17276c8920dac9a5a6eaa7d3e27bb900c3
Ashurov David <ashurdav@mail.ru>
2025-06-10 16:46:03 +0300

    n

---
+++ src/main/java/telran/java57/farm_sharing/FarmSharingApplication.java
@@ -0,0 +1,13 @@
+package telran.java57.farm_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/telran/java57/farm_sharing/FarmSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.java57.farm_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
