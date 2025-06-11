242c088fb2e2ff74b34dbfec5a6a3fd20f6e27fc
Edel-ctrl <edelisakson@gmail.com>
2025-06-11 04:12:21 +0300

    simple-client-crud

--- src/main/java/telran/java57/farm_sharing/config/ServiceConfig.java
+++ src/main/java/telran/java57/farm_sharing/config/ServiceConfig.java
@@ -1,14 +1,19 @@
 package telran.java57.farm_sharing.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfig {
-    
+
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 }
